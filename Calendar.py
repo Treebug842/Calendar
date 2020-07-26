e5512a6ce7594070c2d3a2ae28c47a46d4a8671b
Treebug842 <51908187+Treebug842@users.noreply.github.com>
2020-07-26 15:48:14 +1000

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -64,10 +64,7 @@ class create_days:
 		self.month_name = month_nums.get(month_num)
 		self.col = None
 
-		if month_num == int(currentMonth) and num_in_month == int(currentDay):
-			self.colour = "yellow"
-
-		elif num_in_month in month_colours[(month_num - 1)][0]:
+		if num_in_month in month_colours[(month_num - 1)][0]:
 			self.colour = "firebrick1"
 		elif num_in_month in month_colours[(month_num - 1)][1]:
 			self.colour = "green yellow"
@@ -78,12 +75,25 @@ class create_days:
 
 		frame = "frame{}".format(month_num)
 
-		command = f"self.button = tk.Button({frame}, height=4, width=7, text={num_in_month}, bg='{self.colour}', command=lambda: create_days.button_click({num_in_month}, {self.col}, {self.month_num}, {self.num}))"
+		if month_num == int(currentMonth) and num_in_month == int(currentDay):
+			command = f"self.button = tk.Button({frame}, font='Helvetica 16 bold', wraplength=80, height=3, width=7, text='{num_in_month}      Today', bg='yellow', command=lambda: create_days.button_click({num_in_month}, {self.col}, {self.month_num}, {self.num}))"
+		else:
+			command = f"self.button = tk.Button({frame}, font='Helvetica 16 bold', height=3, width=7, text={num_in_month}, bg='{self.colour}', command=lambda: create_days.button_click({num_in_month}, {self.col}, {self.month_num}, {self.num}))"
 		exec(command)
 
 
 	@staticmethod
 	def button_click(num_in_month, col, month_num, day_num):
+		global selecter_pos
+		if num_in_month in month_colours[(month_num - 1)][0]:
+			selecter_pos = 0
+		elif num_in_month in month_colours[(month_num - 1)][1]:
+			selecter_pos = 1
+		elif num_in_month in month_colours[(month_num - 1)][2]:
+			selecter_pos = 2
+		else:
+			selecter_pos = 3
+
 		popup = tk.Tk()
 		popup.title("Day Entry")
 		popup.geometry("400x300")
@@ -92,8 +102,10 @@ class create_days:
 		frame2 = tk.Frame(popup)
 
 		def switch_to_1():
+			save_data()
 			frame2.pack_forget()
 			frame1.pack()
+			print(month_colours)
 
 		def switch_to_2():
 			frame1.pack_forget()
@@ -107,16 +119,62 @@ class create_days:
 		heading_label = tk.Label(frame1, text=heading, padx=5, pady=5, font='Helvetica 13 bold')
 		heading_label.grid(row=0, column=1, columnspan=3, padx=5, pady=5)
 		
-		textbox = tk.Frame(frame1, borderwidth=3, highlightbackground="black", highlightthickness=1, padx=80, pady=80)
+		textbox = tk.Frame(frame1, borderwidth=3, highlightbackground="black", highlightthickness=1, height=180, width=340)
+		textbox.pack_propagate(0)
 		textbox.grid(row=1, column=1, columnspan=3)
-		note = tk.Label(textbox, text=(day_notes[day_num - 1]))
+		note = tk.Label(textbox, wraplength=340, justify=tk.LEFT, text=(day_notes[day_num - 1]))
 		note.pack()
 
-		edit_button = tk.Button(frame1, command=lambda:switch_to_2())
-		edit_button.grid(row=4, column=0)
+		edit_button = tk.Button(frame1, command=lambda:switch_to_2(), padx=8, pady=3, text="Edit", bg="gray91")
+		edit_button.grid(row=4, column=0, pady=10, columnspan=2, sticky=tk.W)
 
 		frame1.pack()
 
+		def select_colour(colour):
+			global selecter_pos
+			global month_colours
+			if colour == 'red' and num_in_month not in month_colours[(month_num - 1)][0]:
+				month_colours[(month_num - 1)][0].append(num_in_month)
+				selecter_pos = 0
+
+			elif colour == 'green' and num_in_month not in month_colours[(month_num - 1)][1]:
+				month_colours[(month_num - 1)][1].append(num_in_month)
+				selecter_pos = 1
+
+			elif colour == 'blue' and num_in_month not in month_colours[(month_num - 1)][2]:
+				month_colours[(month_num - 1)][2].append(num_in_month)
+				selecter_pos =2
+
+			elif colour == 'gray91':
+				if num_in_month in month_colours[(month_num - 1)][0]:
+					month_colours[(month_num - 1)][0].remove(num_in_month)
+				if num_in_month in month_colours[(month_num - 1)][1]:
+					month_colours[(month_num - 1)][1].remove(num_in_month)
+				if num_in_month in month_colours[(month_num - 1)][2]:
+					month_colours[(month_num - 1)][2].remove(num_in_month)
+				selecter_pos = 3
+
+			selecter.grid_forget()
+			selecter.grid(row=0, column=selecter_pos)
+
+
+
+		colour_box = tk.Frame(frame2)
+
+		colour1_button = tk.Button(colour_box, padx=10, bg="red", command=lambda:select_colour('red'))
+		colour1_button.grid(row=1, column=0)
+		colour2_button = tk.Button(colour_box, padx=10, bg="green", command=lambda:select_colour('green'))
+		colour2_button.grid(row=1, column=1)
+		colour3_button = tk.Button(colour_box, padx=10, bg="blue", command=lambda:select_colour('blue'))
+		colour3_button.grid(row=1, column=2)
+		colour4_button = tk.Button(colour_box, padx=10, bg="gray91", command=lambda:select_colour('gray91'))
+		colour4_button.grid(row=1, column=3)
+
+		# arrow_icon = ImageTk.PhotoImage(Image.open("lib/arrow.png"))
+		selecter = tk.Label(colour_box, text="^")
+		selecter.grid(row=0, column=selecter_pos)
+
+		colour_box.grid(row=3, column=0)
 
 
 		save_button = tk.Button(frame2, command=lambda:switch_to_1())
@@ -154,8 +212,28 @@ october = create_months(31, 4, 10, 275)
 november = create_months(30, 0, 11, 306)
 december = create_months(31, 2, 12, 336)
 
-heading = tk.Label(root, font='Helvetica 22 bold', text="2020", pady=5)
+heading = tk.Label(root, font='Helvetica 22 bold', text="2020", pady=10)
 heading.grid(row=0, column=1, columnspan=3)
+
+heading_frame = tk.Frame(root)
+sunday_Label = tk.Label(heading_frame, text="Sunday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=19, pady=5)
+monday_Label = tk.Label(heading_frame, text="Monday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=19, pady=5)
+teusday_Label = tk.Label(heading_frame, text="Teusday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=17, pady=5)
+wednesday_Label = tk.Label(heading_frame, text="Wednesday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=4, pady=5)
+thursday_Label = tk.Label(heading_frame, text="Thursday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=12, pady=5)
+friday_Label = tk.Label(heading_frame, text="Friday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=25, pady=5)
+saturday_Label = tk.Label(heading_frame, text="Saturday", font='Helvetica 13 bold', borderwidth=1, relief="solid", padx=15, pady=5)
+
+sunday_Label.grid(row=0, column=0)
+monday_Label.grid(row=0, column=1)
+teusday_Label.grid(row=0, column=2)
+wednesday_Label.grid(row=0, column=3)
+thursday_Label.grid(row=0, column=4)
+friday_Label.grid(row=0, column=5)
+saturday_Label.grid(row=0, column=6)
+
+heading_frame.grid(row=1, column=0, columnspan=5, sticky=tk.W)
+
 
 def update_month(choice):
 	global current_frame
@@ -192,6 +270,9 @@ for x in month_nums:
 	month = month_nums.get(x).lower()
 	command = "place_days({}.start_pos, {}.start_day, {}.num_of_days)".format(month, month, month)
 	exec(command)
+
+
+
 
 
 
